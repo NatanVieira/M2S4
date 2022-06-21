ca2d562184c1bafce1a87ac82c8921c56a044752
Natanael Vieira <natanaelvieira130@gmail.com>
2022-06-21 13:43:18 -0300

    M2S4 - Exercicio 01

---
+++ FormularioInscricao/Models/FichaInscricao.cs
@@ -0,0 +1,14 @@
+﻿namespace FormularioInscricao.Models
+{
+    public class FichaInscricao
+    {
+        public string nome;
+        public string idade;
+        public string curso;
+        public string escolaridade;
+        public string dataDeNascimento;
+        public string valorDoCurso;
+        public string valorDeDesconto;
+        public string valorDaMulta;
+    }
+}
---
+++ FormularioInscricao/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("FormularioInscricao")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FormularioInscricao")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("4032b616-bf7e-450c-95eb-676b0db86af2")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
