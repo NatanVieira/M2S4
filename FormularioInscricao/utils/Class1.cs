6b474d89c11ea09cbd215ffdae05aa03baa50a01
Natanael Vieira <natanaelvieira130@gmail.com>
2022-06-21 14:25:13 -0300

    M2S4 - Exercicio 04

---
+++ FormularioInscricao/utils/Class1.cs
@@ -0,0 +1,27 @@
+﻿using FormularioInscricao.Models;
+
+namespace FormularioInscricao.utils
+{
+    public static class FormatacaoTexto
+    {
+        public static string formataTexto(FichaInscricao fichaInscricao)
+        {
+            if(fichaInscricao.valorDeDesconto <= 0)
+            {
+                return $"Nome: {fichaInscricao.nome}, Curso: {fichaInscricao.curso}, Valor Curso: {fichaInscricao.valorDoCurso.ToString("c")}.";
+            }else if(fichaInscricao.valorDeDesconto > 0)
+            {
+                if(fichaInscricao.idade < 18)
+                {
+                    return $"Nome: {fichaInscricao.nome}, Curso: {fichaInscricao.curso}, Valor Curso: {fichaInscricao.valorDoCurso.ToString("c")}, Valor de desconto: {fichaInscricao.valorDeDesconto.ToString("c")}.";
+                }
+                else
+                {
+                    return $"Nome: {fichaInscricao.nome}, Curso: {fichaInscricao.curso}, Valor Curso: {fichaInscricao.valorDoCurso.ToString("c")}, Valor de desconto: {fichaInscricao.valorDeDesconto.ToString("c")}, Idade: {fichaInscricao.idade}.";
+                }
+            }
+            return "";
+
+        }
+    }
+}
