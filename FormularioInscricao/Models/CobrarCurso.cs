1f9dd6f872b31754bb9a759c25006c0a7c89b38b
Natanael Vieira <natanaelvieira130@gmail.com>
2022-06-21 17:10:27 -0300

    M2S4 - Exercicio 06

---
+++ FormularioInscricao/Models/CobrarCurso.cs
@@ -0,0 +1,13 @@
+﻿namespace FormularioInscricao.Models
+{
+    internal class CobrarCurso
+    {
+        private double valorDoCurso;
+        private double valorDaMulta;
+        private double valorDeDesconto;
+
+        public double ValorDoCurso { get; set; }
+        public double ValorDaMulta { get; set; }
+        private double ValorDeDesconto { get; set; }
+    }
+}
