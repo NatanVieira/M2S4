661bc7e3359514f6cd083a54a3b58d8b30c70f73
Natanael Vieira <natanaelvieira130@gmail.com>
2022-06-21 17:14:16 -0300

    M2S4 - Exercicio 07

--- FormularioInscricao/Models/CobrarCurso.cs
+++ FormularioInscricao/Models/CobrarCurso.cs
@@ -5,9 +5,17 @@
         private double valorDoCurso;
         private double valorDaMulta;
         private double valorDeDesconto;
+        private double resultado;
 
         public double ValorDoCurso { get; set; }
         public double ValorDaMulta { get; set; }
-        private double ValorDeDesconto { get; set; }
+        public double ValorDeDesconto { get; set; }
+        public double Resultado { get; set; }
+
+        public void CalcularResultado()
+        {
+            this.resultado = this.valorDaMulta > 0 ? this.valorDoCurso + this.valorDaMulta : this.valorDoCurso - this.valorDeDesconto;
+        }
+
     }
 }
